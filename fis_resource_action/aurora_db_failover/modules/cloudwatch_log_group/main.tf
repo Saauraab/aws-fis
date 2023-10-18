b34afe1989266a9f2ddddbae614c5e9acfde4a65
saauraab <saauraab@amazon.co.uk>
2023-10-18 21:01:10 +0000

    removed unused files

--- fis_resource_action/aurora_db_failover/modules/cloudwatch_log_group/main.tf
+++
@@ -1,7 +0,0 @@
-
-resource "aws_cloudwatch_log_group" "fis_loggroup" {
-  name = var.fis_loggroup_name
-  retention_in_days = var.retention_in_days
-}
-
-
--- fis_resource_action/aurora_db_failover/modules/cloudwatch_log_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "fis_loggroup_name" {
-  description = "The aws region to deploy"
-  type        = string
-}
-
-variable "retention_in_days" {
-  description = "The aws region to deploy"
-  type        = string
-}
