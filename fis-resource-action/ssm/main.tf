a66cf8441e327ee3dd85fca0997a0120ec7d5707
EC2 Default User <ec2-user@ip-172-31-46-181.eu-central-1.compute.internal>
2023-11-02 16:33:25 +0000

    format the tf files

--- fis-resource-action/ssm/main.tf
+++ fis-resource-action/ssm/main.tf
@@ -2,16 +2,16 @@
 ## IAM role created to performt the db failover operation on the aurora db instances instances##
 ##############################################################################################
 module "fis_ssm_iam" {
-  source            = "./modules/created-iam/"
-  role_name         = var.role_name
-  policy_name       = var.policy_name
+  source      = "./modules/created-iam/"
+  role_name   = var.role_name
+  policy_name = var.policy_name
 }
 
 module "fis_ssm" {
   source            = "./modules/fis-ssm/"
-  impacted_az = var.impacted_az
+  impacted_az       = var.impacted_az
   autoscaling_group = var.autoscaling_group
-  assumeRole = var.assumeRole
-  duration = var.duration
-  name = var.name
+  assumeRole        = var.assumeRole
+  duration          = var.duration
+  name              = var.name
 }
